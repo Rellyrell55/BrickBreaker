feeaa0bc59da027b3229416fd9b46e4ee955caf8
Terrell Cooper <TerrellCoop@gmail.com>
2020-12-03 12:54:23 -0700

    test commit

--- app.js
+++ app.js
@@ -31,15 +31,14 @@ function saveNewUser(user){
     onStart()
 }
 
-// function onStart(){ 
-//     newScript = document.createElement('script')
-    
-//     newScript.src = 'game.js'
+function onStart(){ 
+    newScript = document.createElement('script')
+    newScript.src = 'game.js'
 
-//     head.append(newScript)
-//     userForm.remove()
-//     title.remove()
-// }
+    head.append(newScript)
+    userForm.remove()
+    title.remove()
+}
 
 fetch(userURL)
     .then (response => response.json())
